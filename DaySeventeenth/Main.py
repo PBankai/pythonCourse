26ce5ee53db7966005e0c1bd8bb3d2d2d2b481ad
Pedro Estrada <pedro.eshez@gmail.com>
2022-08-02 23:19:12 -0500

    Modifying the readme file adding url

--- DaySeventeenth/Main.py
+++ DaySeventeenth/Main.py
@@ -1,22 +1,7 @@
-# class User:
-#     def __init__(self, user_id, username):
-#         print("Object created")
-#         self.id = user_id
-#         self.username = username
-#         self.followers = 0
-#         self.following = 0
-#
-#     def follow(self, user):
-#         user.followers += 1
-#         self.following += 1
-#
-#
-# user_1 = User(1233, "Pedro")
-# print(user_1.username)
 from question import Question
 from data import question_data
 from Question_Brain import QuizzBrain
-question_bank=[]
+question_bank = []
 for question in question_data:
     question_text = question["text"]
     question_answer = question["answer"]
--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
 hundred day of python course by angela Yu
-on udemy
+on udemy url= https://www.udemy.com/course/100-days-of-code/learn
 we are to do a challenge of python coding on daily basis
 repo, to get a history of my progress
